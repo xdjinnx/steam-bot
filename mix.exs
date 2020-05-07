3496748bac17b99ea60b5d8bcdf23f8611f737c7
Peter Lundberg <peter.lundberg@soundtrackyourbrand.com>
2020-05-07 15:29:24 +0200

    Add mox

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule SteamBot.MixProject do
       {:jason, "~> 1.2"},
       {:tesla, "~> 1.3.0"},
       {:hackney, "~> 1.15.2"},
-      {:bypass, "~> 1.0", only: :test}
+      {:mox, "~> 0.5", only: :test}
     ]
   end
 
